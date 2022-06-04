be6a4e8581cae3939ff5571634763ddc429fe094
ShiraOhana <soshiraohana@gmail.com>
2022-06-04 18:17:07 +0300

    Route

---
+++ my-app/src/pages/Board.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Board() {
+  return (
+    <div>
+      Board
+      <Link to="/board">Board</Link>
+    </div>
+  );
+}
+
+export default Board;
---
+++ my-app/src/pages/Events.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Events() {
+  return (
+    <div>
+      Events
+      <Link to="/events">Events</Link>
+    </div>
+  );
+}
+
+export default Events;
