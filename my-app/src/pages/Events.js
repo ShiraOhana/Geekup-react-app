ee2e45b91d1fb6d451297b11cd9a5791f376e6a6
ShiraOhana <soshiraohana@gmail.com>
2022-06-05 17:39:19 +0300

    Display the api fetch

--- my-app/src/pages/Events.js
+++ my-app/src/pages/Events.js
@@ -3,25 +3,29 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 function Events() {
-  function fetchData() {
-    const [event, setEvent] = useState([]);
+  const [events, setEvent] = useState([]);
 
-    useEffect(() => {
-      const search = async () => {
-        const { data } = await axios.get(
-          "https://629b7c06cf163ceb8d1a51ed.mockapi.io/meetings"
-        );
-        setEvent(data);
-      };
-      search();
-    }, []);
-  }
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get(
+        "https://629b7c06cf163ceb8d1a51ed.mockapi.io/meetings/meetup/"
+      );
+      setEvent(data);
+    };
+    search();
+  }, []);
+
   return (
     <div>
-      <p> Events</p>
-      <div></div>
-
-      <Link to="/home">Events</Link>
+      <div className="card">
+        <h3> {events.title}</h3>
+        <p>{events.date}</p>
+        <p>{events.country}</p>
+        <img src={events.country} />
+        <p>{events.description}</p>
+        <p>Online: {events.online}</p>
+      </div>
+      <Link to="/home">Back Home</Link>
     </div>
   );
 }
