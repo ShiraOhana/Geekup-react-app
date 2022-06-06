61e78b6df55ef739137053ffca2772f22ce509d6
ShiraOhana <soshiraohana@gmail.com>
2022-06-06 10:11:16 +0300

    display

--- my-app/src/pages/Events.js
+++ my-app/src/pages/Events.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import moment from "moment";
 
 function Events() {
   const [events, setEvent] = useState([]);
@@ -10,20 +11,26 @@ function Events() {
       const { data } = await axios.get(
         "https://629b7c06cf163ceb8d1a51ed.mockapi.io/meetings/meetup/"
       );
+      console.log(data);
       setEvent(data);
     };
     search();
   }, []);
 
   return (
-    <div>
-      <div className="card">
-        <h3> {events.title}</h3>
-        <p>{events.date}</p>
-        <p>{events.country}</p>
-        <img src={events.country} />
-        <p>{events.description}</p>
-        <p>Online: {events.online}</p>
+    <div className="container">
+      <select></select>
+      <div className="grid">
+        {events.map((event) => (
+          <div className="card">
+            <h3> {event.title}</h3>
+            <p>{moment(event.date).format("MMMM Do YYYY, h:mm a")}</p>
+            <p>{event.country}</p>
+            <img src={event.img} />
+            <p>{event.description}</p>
+            <p>Online: {event.Online ? "V" : "X"}</p>
+          </div>
+        ))}
       </div>
       <Link to="/home">Back Home</Link>
     </div>
