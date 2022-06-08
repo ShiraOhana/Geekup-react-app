66bd2200276dfbf9cf137d5b0c463940ed5fd094
ShiraOhana <soshiraohana@gmail.com>
2022-06-08 18:19:52 +0300

    fixed navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,18 +8,23 @@ function Home() {
   return (
     <div className="home">
       <header>
-        <img src={require("../assets/logo.png")} className="logo" />
-
-        <ul>
-          <Link to="/" className="nav-links">
-            Home
-          </Link>
-          <Link to="/events" className="nav-links">
-            Events
-          </Link>
-          <Link to="/board" className="nav-links">
-            Board
-          </Link>
+        <ul className="navbar">
+          <li>
+            <img src={require("../assets/logo.png")} className="logo" />
+          </li>
+          <li>
+            <ul>
+              <Link to="/" className="nav-links">
+                Home
+              </Link>
+              <Link to="/events" className="nav-links">
+                Events
+              </Link>
+              <Link to="/board" className="nav-links">
+                Board
+              </Link>
+            </ul>
+          </li>
         </ul>
       </header>
       {/* <Link to="/">Home</Link> */}
