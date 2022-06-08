3493ad4503fad51ac2550845203c5330232dfc86
ShiraOhana <soshiraohana@gmail.com>
2022-06-08 21:22:06 +0300

    moved header to a new component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,27 +9,6 @@ function Home() {
   return (
     <div className="home">
       <Header />
-      {/* <header>
-        <ul className="navbar">
-          <li>
-            <img src={require("../assets/logo.png")} className="logo" />
-          </li>
-          <li>
-            <ul>
-              <Link to="/" className="nav-links">
-                Home
-              </Link>
-              <Link to="/events" className="nav-links">
-                Events
-              </Link>
-              <Link to="/board" className="nav-links">
-                Board
-              </Link>
-            </ul>
-          </li>
-        </ul>
-      </header> */}
-
       <div className="explore-homepage">
         <p> Check out our events</p>
         <button>Explore</button>{" "}
