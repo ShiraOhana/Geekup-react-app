1331de17b43e23adf94126985090ed5bec44900e
ShiraOhana <soshiraohana@gmail.com>
2022-06-08 22:04:04 +0300

    explore btn

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,17 @@ import React from "react";
 import "../App.css";
 import "../assets/logo.png";
 import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="home">
       <Header />
       <div className="explore-homepage">
-        <p> Check out our events</p>
-        <button>Explore</button>{" "}
+        <p> Check out new events</p>{" "}
+        <Link to="/events" className="explore-link">
+          Explore{" "}
+        </Link>
       </div>
     </div>
   );
